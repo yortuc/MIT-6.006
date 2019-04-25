88a7218d88bb65f60f55f1b03ad38d9cbe66c919
yortuc <yortucboylu@gmail.com>
2019-04-25 07:04:59 +0200

    lec1-b: peak finding binary search

--- peak_finding.py
+++ peak_finding.py
@@ -1,14 +1,31 @@
+import math
+import sys
+
 # PEEK FINDING 
 # One dimensional
 
 # Naive approach θ(n)
 def find_peak_naive(arr):
-    for i in range(0, len(arr)):
+    for i in range(len(arr)):
         x = arr[i]
-        left = arr[i-1]
-        right = arr[i+1]
+        left = arr[i-1] if i>0 else -sys.maxsize
+        right = arr[i+1] if i<len(arr)-1 else -sys.maxsize
 
         if x >= left and x >= right:
-            return i
+            return x
 
+# recursive binary search θ(logn)
+def find_peak_binary_search(arr):
+    h = math.floor(len(arr) / 2)
+    x = arr[h]
+    left = arr[h-1] if h>0 else -sys.maxsize
+    right = arr[h+1] if h<len(arr)-1 else -sys.maxsize
+    
+    if x >= left and x >= right:
+        return x
+    elif x>= left: # take right
+        par_arr = arr[h:]
+    elif x>= right: # take left
+        par_arr = arr[:h]
 
+    return find_peak_binary_search(par_arr)
--- test_peak_finding.py
+++ test_peak_finding.py
@@ -1,5 +1,17 @@
-from peak_finding import find_peak_naive
+from peak_finding import find_peak_naive, find_peak_binary_search
 
 def test_find_peak_naive():
     arr = [1, 2, 3, 4, 5, 6, 4, 2]
-    assert find_peak_naive(arr) == 5 # position
+    assert find_peak_naive(arr) == 6
+
+
+def test_find_peak_binary_search():
+    arr_one_element = [2]
+    arr_peak_middle = [1, 2, 3, 4, 5, 6, 4, 2]
+    arr_peak_end = [5, 6, 7, 8]
+    arr_peak_start = [12, 5, 3]
+    
+    assert find_peak_binary_search(arr_one_element) == 2
+    assert find_peak_binary_search(arr_peak_middle) == 6
+    assert find_peak_binary_search(arr_peak_end) == 8
+    assert find_peak_binary_search(arr_peak_start) == 12
